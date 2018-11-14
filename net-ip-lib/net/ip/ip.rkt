26f961512e12b173710eab78e7b642d7fe218b31
Bogdan Popa <popa.bogdanp@gmail.com>
2018-11-14 09:49:22 +0200

    refactor(ipv4): make ipv4 opaque

--- net-ip-lib/net/ip/ip.rkt
+++ net-ip-lib/net/ip/ip.rkt
@@ -9,9 +9,13 @@
           [ip-address? (-> any/c boolean?)]
           [ip-address-dec (->* (ip-address?) (exact-integer?) ip-address?)]
           [ip-address-inc (->* (ip-address?) (exact-integer?) ip-address?)]
-          [ip-address->string (-> ip-address? string?)]))
+          [ip-address->number (-> ip-address? number?)]
+          [ip-address->string (-> ip-address? string?)]
+          [ip-address->version (-> ip-address? number?)]))
 
 (define-generics ip-address
   (ip-address-dec ip-address [n])
   (ip-address-inc ip-address [n])
+  (ip-address->version ip-address)
+  (ip-address->number ip-address)
   (ip-address->string ip-address))
--- net-ip-lib/net/ip/ipv4.rkt
+++ net-ip-lib/net/ip/ipv4.rkt
@@ -6,21 +6,35 @@
          "ip.rkt")
 
 (provide (contract-out
-          [struct ipv4-address ((value exact-integer?))]
+          [ipv4-address? (-> any/c boolean?)]
+          [ipv4-address (-> exact-integer? ipv4-address?)]
           [parse-ipv4 (-> string? ipv4-address?)]))
 
 (define MIN-IPV4-VALUE 0)
 (define MAX-IPV4-VALUE (sub1 (expt 256 4)))
 
-(struct ipv4-address (value)
-  #:transparent
+(define (ipv4-address=? a b recursive-equal?)
+  (= (ipv4-address-value a)
+     (ipv4-address-value b)))
 
+(define (ipv4-hash-1 addr recursive-equal-hash?)
+  (ipv4-address-value addr))
+
+(define (ipv4-hash-2 addr recursive-equal-hash?)
+  (ipv4-address-value addr))
+
+(struct ipv4-address (value)
   #:guard
   (lambda (value name)
     (unless (and (>= value MIN-IPV4-VALUE)
                  (<= value MAX-IPV4-VALUE))
       (raise-argument-error 'ipv4-address "IPv4 addresses must be between 0 and 256^4-1" value))
     value)
+
+  #:methods gen:equal+hash
+  [(define equal-proc ipv4-address=?)
+   (define hash-proc  ipv4-hash-1)
+   (define hash2-proc ipv4-hash-2)]
 
   #:methods gen:ip-address
   [(define (ip-address-dec addr [n 1])
@@ -29,13 +43,18 @@
    (define (ip-address-inc addr [n 1])
      (ipv4-address (+ (ipv4-address-value addr) n)))
 
+   (define (ip-address->number addr)
+     (ipv4-address-value addr))
+
    (define (ip-address->string addr)
      (let loop ([n (ipv4-address-value addr)]
                 [s '()])
        (if (= 4 (length s))
            (string-join (map number->string s) ".")
            (loop (quotient n 256)
-                 (cons (remainder n 256) s)))))])
+                 (cons (remainder n 256) s)))))
+
+   (define (ip-address->version addr) 4)])
 
 (define (parse-ipv4 ip)
   (define octets (map string->number (string-split ip ".")))
